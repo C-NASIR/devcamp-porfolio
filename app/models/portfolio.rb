38805f82d7a80b3c7350c8f2c94bb8371993a735
C NASIR <cnasir22.0@gmail.com>
2020-09-12 17:54:17 -0500

    Implemented custom scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,16 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    # this methods if for pure demonstration
+    # you can filter data in two ways 
+    def self.reactjs
+        where(subtitle: "React JS")      
+    end
+    # and then call inside the action like   Portfolio.angular instead of .all
+
+    #or 
+
+    scope :ruby_on_rails, -> {where(subtitle: "React JS")}
+    #which is same as option one, Portfolio.ruby_on_rails 
+
 end
